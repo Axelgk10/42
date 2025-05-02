a5e567e2aa41a15e9f3b3727a4c81522488ed4d9
Axelgk10 <axelkonert@gmail.com>
2025-05-02 11:32:53 +0200

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,78 +1,116 @@
 #include "libft.h"
-
-//FUNCION EXTRA
-static int count_words(const  char *s, char c)
-{
-    int count;
-    int in_word;
-    count = 0;
-    in_word = 0;
-    while (*s)
-    {
-        if (*s == c && !in_word)
-        {
-            in_word = 1;
-            count++;
-        }
-        else if (*s == c)
-        {
-            in_word = 0;
-            s++;
-        }
-        return (count);
-    }
-}
-
 // FUNCION EXTRA
-static char *word_dup(const char *s, int start, int end)
+static int	count_words(const char *s, char c)
 {
-    int i;
-    i = 0;
-    char *word = malloc(end - start + 1);
-    while (start < end)
-    {
-        word[i] = s[start];
-        i++;
-        start++;
-    }
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s && *s != c)
+		{
+			count++;
+			while (*s && *s != c)
+				s++;
+		}
+	}
+	return (count);
+}
+// FUNCION EXTRA
+static char	*substr(const char *s, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	word = malloc(end - start + 1);
+	i = 0;
+	if (!word)
+		return (NULL);
+	while (start < end)
+		word[i++] = s[start++];
+	word[i] = '\0';
+	return (word);
+}
+// FUNCION EXTRA
+static void	free_all(char **res, int i)
+{
+	while (--i)
+		free (res[i]);
+	free (res);
+}
+// FUNCION PRINCUPAL
+static int	fill_split(char **res, const char *s, char c)
+{
+	int		i;
+	int		start;
+	int		end;
+	int		word_i;
+
+	i = 0;
+	word_i = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		end = i;
+		if (end > start)
+		{
+			res[word_i] = substr(s, start, end);
+			if (!res[word_i])
+				return (free_all(res, word_i), 0);
+			word_i++;
+		}
+	}
+	res[word_i] = NULL;
+	return (1);
+}
+// FUNCION EXTRA
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+
+	res = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!res)
+		return (NULL);
+	if (!fill_split(res, s, c))
+		return (NULL);
+	return (res);
 }
 
-// FUNCION PRINCIPAL
-char **ft_split(char const *s, char c)
+int main()
 {
+    char str[] = "Hola, soy un ejemplo de cadena!";
+    char delimiter = ' ';  // Separador en este caso es el espacio
     char **result;
-    int i;
-    int j;
-    int start;
-    int end;
+    int i = 0;
 
-    i = 0;
-    j = 0;
-    start = -1;
-    words = 0;
-    words = count_words(s, c);
-    result = malloc((words + 1) * sizeof(char *));
-    if (!s)
-    {
-        return (NULL);
-    }
+    result = ft_split(str, delimiter);
     if (!result)
     {
-        return (NULL);
+        printf("Error al dividir la cadena\n");
+        return 1;
     }
-    while (s[i])
+
+    // Imprimir las subcadenas resultantes
+    while (result[i])
     {
-        if (s[i] != c && start < 0)
-        {
-            start = i;
-        }
-        else if ((s[i] == c || s[i + 1] == '\0') && start >= 0)
-        {
-            result[j++] = word_dup(s[i] == c) ? s[i] : s[i + 1];
-            start = -1;
-        }
+        printf("Subcadena %d: %s\n", i + 1, result[i]);
         i++;
     }
-    result[j] = NULL;
-    return (result);   
+
+    // Liberar la memoria asignada
+    i = 0;
+    while (result[i])
+    {
+        free(result[i]);
+        i++;
+    }
+    free(result);
+
+    return 0;
 }
